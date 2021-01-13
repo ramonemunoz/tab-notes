49957d9f7df1aac9131b57c7064d6d30c21b6557
Ramon Munoz <ramonmunoz1027@gmail.com>
2021-01-12 22:49:28 -0800

    added options to the medium editor component

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import MediumEditor from './node_modules/medium-editor/dist/js/medium-editor.min.js';
 
 var editor = new MediumEditor('.editable', {
-    toolbar: false
+    toolbar: false,
+    placeholder: {
+        text: 'type',
+        hideOnClick: false,
+    }
 });
